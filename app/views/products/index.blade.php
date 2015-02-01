ea27b8de09bc0aa30b7aef0591a21907c87f9171
Bob Sun <doppelgange@gmail.com>
2015-02-01 18:41:37 +1300

    Amend the products index view

--- app/views/products/index.blade.php
+++ app/views/products/index.blade.php
@@ -8,36 +8,34 @@
 <table class="table table-striped table-bordered table-hover table-condensed">
 	<thead>
 		<tr>
-			<th> productID </th>
-			<th> name </th>
-			<th> code </th>
-			<th> ean </th>
-			<th> nameCN </th>
-			<th> supplierID </th>
-			<th> supplierName </th>
-			<th> groupID </th>
-			<th> groupName </th>
-			<th> categoryID </th>
-			<th> categoryName </th>
-			<th> seriesID </th>
+			<th> ID </th>
+			<th> Name </th>
+			<th> Code </th>
+			<th> EAN </th>
+			<th> Name CN </th>
+			<!-- <th> supplierID </th>
+			<th> supplierName </th> -->
+			<!-- <th> groupID </th>
+			<th> Group </th>
+			<th> categoryID </th> -->
+			<th> Category</th>
+			<!-- <th> seriesID </th>
 			<th> seriesName </th>
-			<th> unitID </th>
-			<th> unitName </th>
-			<th> price </th>
-			<th> priceWithVat </th>
-			<th> cost </th>
-			<th> status </th>
-			<th> active </th>
-			<th> displayedInWebshop </th>
-			<th> vatrate </th>
+			<th> unitID </th> -->
+			<th> Unit </th>
+			<th> Price(VAT), Price, Cost</th>
+			<th> Status </th>
+			<!-- <th> active </th> -->
+			<th> Webshop </th>
+			<!-- <th> vatrate </th>
 			<th> countryOfOriginID </th>
 			<th> brandName </th>
-			<th> netWeight </th>
+			<th> netWeight </th> -->
 			<th> grossWeight </th>
-			<th> volume </th>
-			<th> longdesc </th>
-			<th> erplyAdded </th>
-			<th> erplyLastModified </th>
+			<!-- <th> VOL </th> -->
+			<th> Desc </th>
+			<!-- <th> erplyAdded </th> -->
+			<th> erply Last Modified </th>
 		</tr>
 	</thead>
 	<tbody>
@@ -48,30 +46,32 @@
 			<td> {{ $product -> code }} </td>
 			<td> {{ $product -> ean }} </td>
 			<td> {{ $product -> nameCN }} </td>
-			<td> {{ $product -> supplierID }} </td>
+			<!-- <td> {{ $product -> supplierID }} </td> 
 			<td> {{ $product -> supplierName }} </td>
 			<td> {{ $product -> groupID }} </td>
 			<td> {{ $product -> groupName }} </td>
-			<td> {{ $product -> categoryID }} </td>
+			<td> {{ $product -> categoryID }} </td> -->
 			<td> {{ $product -> categoryName }} </td>
-			<td> {{ $product -> seriesID }} </td>
+			<!-- <td> {{ $product -> seriesID }} </td>
 			<td> {{ $product -> seriesName }} </td>
-			<td> {{ $product -> unitID }} </td>
+			<td> {{ $product -> unitID }} </td> -->
 			<td> {{ $product -> unitName }} </td>
-			<td> {{ $product -> price }} </td>
-			<td> {{ $product -> priceWithVat }} </td>
-			<td> {{ $product -> cost }} </td>
+			<td> 
+				<div> {{ $product -> priceWithVat }}</div>
+				<div> {{ $product -> price }} </div>
+				<div> {{ $product -> cost }} </div>
+			</td>
 			<td> {{ $product -> status }} </td>
-			<td> {{ $product -> active }} </td>
+			<!-- <td> {{ $product -> active }} </td> -->
 			<td> {{ $product -> displayedInWebshop }} </td>
-			<td> {{ $product -> vatrate }} </td>
+			<!-- <td> {{ $product -> vatrate }} </td>
 			<td> {{ $product -> countryOfOriginID }} </td>
 			<td> {{ $product -> brandName }} </td>
-			<td> {{ $product -> netWeight }} </td>
+			<td> {{ $product -> netWeight }} </td> -->
 			<td> {{ $product -> grossWeight }} </td>
-			<td> {{ $product -> volume }} </td>
+			<!-- <td> {{ $product -> volume }} </td> -->
 			<td> {{ $product -> longdesc }} </td>
-			<td> {{ $product -> erplyAdded }} </td>
+			<!-- <td> {{ $product -> erplyAdded }} </td> -->
 			<td> {{ $product -> erplyLastModified }} </td>
 	    </tr>
 	@endforeach
