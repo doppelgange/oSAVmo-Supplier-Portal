e7c9adfa0412f2c4060972bae6a6163e738fd6c5
Bob Sun <doppelgange@gmail.com>
2015-02-24 14:09:21 +1300

    Add init sql

---
+++ Source Files/init sql.sql
@@ -0,0 +1,19 @@
+INSERT INTO `properties` (`name`, `key`, `value`, `remarks`)
+VALUES
+	('EnvErplyClientCode', 'production', '284954', 'Add by Bob'),
+	('EnvErplyUsername', 'production', 'bsun', 'Add by Bob'),
+	('EnvErplyPassword', 'production', '3792565Jj', 'Add by Bob'),
+	('EnvErplyUrl', 'production', 'https://284954.erply.com/api/', 'Add by Bob'),
+	('EnvErplyClientCode', 'qa', '400206', 'Add by Bob'),
+	('EnvErplyUsername', 'qa', 'doppelganger', 'Add by Bob'),
+	('EnvErplyPassword', 'qa', '3792565Jj', 'Add by Bob'),
+	('EnvErplyUrl', 'qa', 'https://400206.erply.com/api/	', 'Add by Bob'),
+	('ErplyPricelistID', 'production', '8', 'Add by Bob'),
+	('ErplyPricelistID', 'qa', '8', 'Add by Bob'),
+	('ErplyCreatorID', 'production', '601', 'Add by Bob'),
+	('ErplyCreatorID', 'qa', '2608', 'Add by Bob'),
+	('ErplyReasonID', 'production', '5', 'Add by Bob'),
+	('ErplyReasonID', 'qa', '5', 'Add by Bob'),
+	('ErplyWarehouseID', 'production', '1', 'Add by Bob'),
+	('ErplyWarehouseID', 'qa', '1', 'Add by Bob'),
+	('EnvIndicator', 'CurrentEnvironment', 'qa', 'Add by Bob');
